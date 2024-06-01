ed668308e3ca6ff3b6bed542b4ef29a1258e99ab
Pankaj Chaudhary <pankajchaudhary281@gmail.com>
2024-06-01 17:22:51 +1000

    Convert GRPC API to rest api via jsontranscoding package

--- MyGrpcService/Program.cs
+++ MyGrpcService/Program.cs
@@ -13,11 +13,24 @@ builder.Services.AddDbContext<AppDbContext>(db => db.UseSqlServer(conn));
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-// builder.Services.AddGrpc().AddJsonTranscoding();
+builder.Services.AddGrpc().AddJsonTranscoding(); 
+builder.Services.AddGrpcSwagger();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new Microsoft.OpenApi.Models.OpenApiInfo { Title = "gRPC Api via transcoding", Version = "v1" });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+if (app.Environment.IsDevelopment())
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Grpc Transcoding API V1");
+    });
+
+
 app.MapGrpcService<CategoryGrpcService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
